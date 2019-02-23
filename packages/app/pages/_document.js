17d37826031cc385ad7b785fccf2917168922c9e
Marius Balaj <balajmarius93@gmail.com>
2019-02-23 08:27:57 +0200

    Removed Typescript

--- packages/app/pages/_document.js
+++ packages/app/pages/_document.js
@@ -23,7 +23,6 @@ const GlobalStyles = createGlobalStyle`
     font: inherit;
     vertical-align: baseline;
   }
-  /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure, 
   footer, header, hgroup, menu, nav, section {
     display: block;
