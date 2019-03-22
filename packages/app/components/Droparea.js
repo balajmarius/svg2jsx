fefd3b9717da10f13ea056809e171c9109284cc9
Balaj Marius <balajmarius93@gmail.com>
2019-03-22 19:04:01 +0200

    Create Droparea.js

---
+++ packages/app/components/Droparea.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { useDropzone } from 'react-dropzone';
+
+import { noop } from '../shared/utils';
+
+const DropRoot = styled.div`
+  outline: 0;
+`;
+
+function Droparea({ children, onDrop }) {
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  const rootProps = getRootProps();
+  const inputProps = getInputProps();
+
+  return (
+    <DropRoot {...rootProps} onClick={noop}>
+      <input {...inputProps} />
+      {children(rootProps)}
+    </DropRoot>
+  );
+}
+
+Droparea.propTypes = {
+  onDrop: PropTypes.func.isRequired,
+};
+
+export default Droparea;
