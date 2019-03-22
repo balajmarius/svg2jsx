0d09bf5c6f7388b7120ced9a498311f84f960da2
Balaj Marius <balajmarius93@gmail.com>
2019-03-22 21:45:13 +0200

    Added more colors and editor theme

--- packages/app/shared/constants.js
+++ packages/app/shared/constants.js
@@ -7,6 +7,8 @@ export const color = {
   purple: '#5522e6',
   green: '#21d179',
   red: '#f10c4a',
+  codGrey: '#1C1C1C',
+  dustyGrey: '#949494',
 };
 
 export const fontSize = {
@@ -22,4 +24,49 @@ export const fontFamily = {
 
 export const transition = {
   easeInOut: '300ms ease-in-out',
+};
+
+export const theme = {
+  plain: {
+    color: color.white,
+    backgroundColor: color.black,
+  },
+  styles: [
+    {
+      types: ['prolog', 'constant', 'builtin'],
+      style: { color: 'rgb(189, 147, 249)' },
+    },
+    {
+      types: ['inserted', 'function'],
+      style: { color: 'rgb(80, 250, 123)' },
+    },
+    {
+      types: ['deleted'],
+      style: { color: 'rgb(255, 85, 85)' },
+    },
+    {
+      types: ['changed'],
+      style: { color: 'rgb(255, 184, 108)' },
+    },
+    {
+      types: ['punctuation', 'symbol'],
+      style: { color: 'rgb(248, 248, 242)' },
+    },
+    {
+      types: ['string', 'char', 'tag', 'selector'],
+      style: { color: 'rgb(255, 121, 198)' },
+    },
+    {
+      types: ['keyword', 'variable'],
+      style: { color: 'rgb(189, 147, 249)', fontStyle: 'italic' },
+    },
+    {
+      types: ['comment'],
+      style: { color: 'rgb(98, 114, 164)' },
+    },
+    {
+      types: ['attr-name'],
+      style: { color: 'rgb(241, 250, 140)' },
+    },
+  ],
 };
