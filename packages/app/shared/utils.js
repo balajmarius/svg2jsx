0ab251aca110241687230681957e13cf5f1b6e32
Balaj Marius <balajmarius93@gmail.com>
2019-04-02 11:59:49 +0300

    Create readFile util

--- packages/app/shared/utils.js
+++ packages/app/shared/utils.js
@@ -1,17 +1,43 @@
 import { color } from './constants';
 
-export const noop = () => {};
+export function noop() {}
 
-export const stripedBackground = (direction, backgroundColor, stripeColor = color.black) => `
-  linear-gradient(
-    to ${direction},
-    ${backgroundColor},
-    ${backgroundColor} calc(50% - 1px),
-    ${stripeColor} calc(50% - 1px),
-    ${stripeColor} calc(50% + 1px),
-    ${backgroundColor} calc(50% + 1px),
-    ${backgroundColor}
-  );
-`;
+export function post(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      data,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
 
-export const readFile = () => {};
+export function readFile(file) {
+  const fileReader = new FileReader();
+
+  return new Promise((resolve, reject) => {
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = () => {
+      reject(new DOMException('Problem parsing file.'));
+    };
+    fileReader.readAsText(file);
+  });
+}
+
+export function stripedBackground(direction, backgroundColor, stripeColor = color.black) {
+  return `
+    linear-gradient(
+      to ${direction},
+      ${backgroundColor},
+      ${backgroundColor} calc(50% - 1px),
+      ${stripeColor} calc(50% - 1px),
+      ${stripeColor} calc(50% + 1px),
+      ${backgroundColor} calc(50% + 1px),
+      ${backgroundColor}
+    );
+  `;
+}
