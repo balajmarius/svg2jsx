c4bb2f01cf52ebf1a3b5657c286400e2b25bb36f
Balaj Marius <balajmarius93@gmail.com>
2019-07-18 17:14:35 +0300

    Add httpStatusCode

--- packages/app/pages/api/transform.ts
+++ packages/app/pages/api/transform.ts
@@ -1,14 +1,16 @@
 import transform from '@svg2jsx/transform';
 import { NextApiRequest, NextApiResponse } from 'next-server/dist/lib/utils';
 
+import httpStatusCode from '../../helpers/httpStatusCode';
+
 export default async function (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> {
   try {
     const jsx = await transform(req.body.svg, req.body.config);
-    res.send({ jsx });
+    res.status(httpStatusCode.OK).send({ jsx });
   } catch (error) {
-    res.status(500).send({ error });
+    res.status(httpStatusCode.BAD_REQUEST);
   }
 }
