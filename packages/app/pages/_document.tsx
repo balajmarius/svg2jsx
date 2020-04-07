0b241d3ac26d53eaa5814a8b4f088bd9b77ff60a
Balaj Marius <balajmarius93@gmail.com>
2020-04-07 17:02:19 +0300

    Fix style flickering

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -1,19 +1,19 @@
-import React, { Fragment, ReactElement } from 'react';
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import { Fragment } from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import Document from 'next/document';
 
-class Root extends Document<DocumentInitialProps> {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+class Root extends Document {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const initialRenderPage = ctx.renderPage;
+    const originalRenderPage = ctx.renderPage;
 
     try {
-      const initialProps = await Document.getInitialProps(ctx);
-
       ctx.renderPage = (): any =>
-        initialRenderPage({
-          enhanceApp: (App: any) => (props: unknown): ReactElement => sheet.collectStyles(<App {...props} />),
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: unknown): unknown => sheet.collectStyles(<App {...props} />),
         });
+
+      const initialProps = await Document.getInitialProps(ctx);
 
       return {
         ...initialProps,
