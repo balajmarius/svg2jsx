6cb7eef1056895021ebcf0fb43746d6fa145f1ea
Balaj Marius <balajmarius@gmail.com>
2024-10-29 14:02:55 +0200

    feat: better spin icon

--- src/components/SvgIcon/SvgIconPending.tsx
+++ src/components/SvgIcon/SvgIconPending.tsx
@@ -5,26 +5,17 @@ import { SvgIcon, type SvgIconProps } from "@/components/SvgIcon";
 export interface SvgIconPendingProps extends SvgIconProps {}
 
 export const SvgIconPending: React.FC<SvgIconPendingProps> = (props) => (
-  <SvgIcon viewBox="0 0 100 100" {...props}>
+  <SvgIcon viewBox="0 0 100 100" className="animate-spin" {...props}>
     <circle
       cx="50"
       cy="50"
-      r="32"
+      r="24"
       fill="none"
       stroke="currentColor"
       strokeDasharray="50.26548245743669 50.26548245743669"
       strokeLinecap="round"
-      strokeWidth="16"
-    >
-      <animateTransform
-        attributeName="transform"
-        dur="1s"
-        keyTimes="0;1"
-        repeatCount="indefinite"
-        type="rotate"
-        values="0 50 50;360 50 50"
-      />
-    </circle>
+      strokeWidth="8"
+    />
   </SvgIcon>
 );
 
