7d1e42ef80777179f3cbab9b806583ad5ccf758d
Balaj Marius <balajmarius@gmail.com>
2026-07-21 10:33:20 +0300

    Pin Node.js 24 runtime

--- src/pages/api/lib/reactify.ts
+++ src/pages/api/lib/reactify.ts
@@ -1,30 +1,21 @@
-import template from "lodash.template";
 import { format } from "prettier";
 
 import type { Options } from "@/utils/types";
 
-const defaultReactComponent = `
-  import * as React from "react";
-
-  <%= definitions %> = (props) => (
-    <%= svg %>
-  );
-
-  <%= exports %>
-`;
-
 export const reactify = async (svg: string, { typescript, memo, jsxSingleQuote }: Options) => {
-  const render = template(defaultReactComponent);
-
   const parser = typescript ? "typescript" : "babel";
   const definitions = typescript ? "const SvgIcon: React.FC<React.SVGProps<SVGElement>>" : "const SvgIcon";
   const exports = memo ? "export default React.memo(SvgIcon);" : "export default SvgIcon;";
 
-  const component = render({
-    svg,
-    exports,
-    definitions,
-  });
+  const component = `
+    import * as React from "react";
+
+    ${definitions} = (props) => (
+      ${svg}
+    );
+
+    ${exports}
+  `;
 
   return await format(component, {
     parser,
